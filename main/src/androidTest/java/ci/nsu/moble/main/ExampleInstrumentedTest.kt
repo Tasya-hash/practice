063a1c31909b528cd29b6627bcf020818b4163d6
MaksimovskayaTE <maksimovskayate@mer.ci.nsu.ru>
2026-02-25 16:15:32 +0700

    Fixed files. It was "moble", not "mobile"

--- main/src/androidTest/java/ci/nsu/moble/main/ExampleInstrumentedTest.kt
+++ main/src/androidTest/java/ci/nsu/moble/main/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package ci.nsu.moble.main
+package ci.nsu.moble.le.main
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
--- main/src/main/java/ci/nsu/moble/main/MainActivity.kt
+++ main/src/main/java/ci/nsu/moble/main/MainActivity.kt
@@ -12,6 +12,32 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import ci.nsu.moble.main.ui.theme.PracticeTheme
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.Column
+import androidx.compose.ui.unit.dp
+import androidx.compose.material3.TextField
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.ui.graphics.Color
+import androidx.compose.material3.ButtonDefaults
+import android.util.Log
+import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+
+
+
+val colorMap = mapOf(
+    "red" to Color.Red,
+    "green" to Color.Green,
+    "blue" to Color.Blue,
+    "yellow" to Color.Yellow,
+    "black" to Color.Black
+)
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,16 +46,65 @@ class MainActivity : ComponentActivity() {
         setContent {
             PracticeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Surface(
+                        modifier = Modifier.padding(innerPadding),
+                        color = Color.White
+                    ) {
+                        ColorScreen()
+                    }
                 }
             }
         }
     }
 }
 
+@Composable
+
+fun ColorScreen(){
+    var text by remember { mutableStateOf("")}
+    var buttonColor by remember {mutableStateOf(Color.Gray)}
+
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ){
+        TextField(
+            value = text,
+            onValueChange = {newValue ->
+                text = newValue
+            },
+            label = {Text("Введите цвет")},
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            colors = ButtonDefaults.buttonColors(containerColor = buttonColor),
+            onClick = {
+                val foundColor = colorMap[text.lowercase()]
+
+                if (foundColor != null){
+                    buttonColor = foundColor
+                } else{
+                    Log.d("ColorSearch", "Цвет \"$text\" не найден")
+                }
+            }){
+            Text("Найти цвет")
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Text(
+            text="Доступные цвета:",
+            color = Color.Black)
+
+        colorMap.keys.forEach { colorName ->
+            Text(
+                text = colorName,
+                color = Color.Black)
+        }
+    }
+}
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
